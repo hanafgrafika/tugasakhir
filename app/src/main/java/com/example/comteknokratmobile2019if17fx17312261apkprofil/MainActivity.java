8bdb04946875150395c7c6fe1c3ab1e0d8ecbf12
root <hanaf.grafika@gmail.com>
2020-01-19 22:24:20 +0700

    Fix Upload Tugas Akhir

--- app/src/main/java/com/example/comteknokratmobile2019if17fx17312261apkprofil/MainActivity.java
+++ app/src/main/java/com/example/comteknokratmobile2019if17fx17312261apkprofil/MainActivity.java
@@ -1,14 +1,75 @@
 package com.example.comteknokratmobile2019if17fx17312261apkprofil;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView myPict;
+    Button editph, save;
+    Animation atg, atg2, atg3;
+    private Button  btn2;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        //animation
+
+        //animation declaration
+        atg = AnimationUtils.loadAnimation(this,R.anim.atg);
+        atg2 = AnimationUtils.loadAnimation(this,R.anim.atg2);
+        atg3 = AnimationUtils.loadAnimation(this,R.anim.atg3);
+
+        //animation Pict
+        myPict = findViewById(R.id.myPict);
+        myPict.startAnimation(atg);
+
+
+
+        //end.animation
+
+        btn2 = (Button) findViewById(R.id.btn2);
+
+//        btn1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                addFragment(new fragmentone(), false, "one");
+//            }
+//        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addFragment(new mapfragment(), false, "one");
+            }
+        });
+
     }
+
+    public void addFragment(Fragment fragment, boolean addToBackStack, String tag) {
+        FragmentManager manager = getSupportFragmentManager();
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (addToBackStack) {
+            ft.addToBackStack(tag);
+        }
+        ft.replace(R.id.container_frame_back, fragment, tag);
+        ft.commitAllowingStateLoss();
+    }
+
+
 }
--- app/src/main/java/com/example/comteknokratmobile2019if17fx17312261apkprofil/mapfragment.java
+++ app/src/main/java/com/example/comteknokratmobile2019if17fx17312261apkprofil/mapfragment.java
@@ -1,4 +1,99 @@
 package com.example.comteknokratmobile2019if17fx17312261apkprofil;
 
-public class mapfragment {
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class mapfragment extends Fragment {
+
+    private Button btnback;
+
+    public mapfragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.mapfragment, container, false);
+        btnback = (Button) rootView.findViewById(R.id.btnback);
+
+        btnback.setOnClickListener(new View.OnClickListener() {
+    @Override
+        public void onClick(View v) {
+        Intent goEdit = new Intent(getActivity(), MainActivity.class);
+        startActivity(goEdit);}});
+
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.frg);  //use SuppoprtMapFragment for using in fragment instead of activity  MapFragment = activity   SupportMapFragment = fragment
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap mMap) {
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+                mMap.clear(); //clear old markers
+
+                CameraPosition googlePlex = CameraPosition.builder()
+                        .target(new LatLng(-5.374945, 105.244373))
+                        .zoom(100)
+                        .bearing(0)
+                        .tilt(45)
+                        .build();
+
+                mMap.animateCamera(CameraUpdateFactory.newCameraPosition(googlePlex), 10000, null);
+
+                mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(-5.374945, 105.244373))
+                        .title("Kholid Aini")
+                        .icon(bitmapDescriptorFromVector(getActivity(),R.drawable.spider)));
+
+
+            }
+
+        });
+
+
+
+
+        return rootView;
+    }
+
+    private BitmapDescriptor bitmapDescriptorFromVector(Context context, int vectorResId) {
+        Drawable vectorDrawable = ContextCompat.getDrawable(context, vectorResId);
+        vectorDrawable.setBounds(0, 0, vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight());
+        Bitmap bitmap = Bitmap.createBitmap(vectorDrawable.getIntrinsicWidth(), vectorDrawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        vectorDrawable.draw(canvas);
+        return BitmapDescriptorFactory.fromBitmap(bitmap);
+    }
+
 }
+
