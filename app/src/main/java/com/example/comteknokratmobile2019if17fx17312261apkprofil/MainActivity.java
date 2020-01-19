202d04cc8cd4c3888d29040bd7f79bcf3c377f84
root <hanaf.grafika@gmail.com>
2020-01-19 10:30:08 +0700

    upload tugas akhir

---
+++ app/src/main/java/com/example/comteknokratmobile2019if17fx17312261apkprofil/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.comteknokratmobile2019if17fx17312261apkprofil;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
